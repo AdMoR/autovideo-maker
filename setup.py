7a89b975c90b4f0b7e5a5ae7f6446e5ee4c6d734
amor <adrien_morvan@hotmail.fr>
2023-07-29 11:58:17 +0200

    debug

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="scripts",
+    name="autovideo",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
     version=0.1,
